a194a1d5fe317ea97c53645e1c4994d6e573f283
Nokraker <valkovivko@gmail.com>
2023-07-12 15:23:35 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <nav className="navigation">
           <Link className="navigation-logo" href="/">
-            LOGO/HOME
+            HOME
           </Link>
 
           <Link className="navigation-link" href="/about">
